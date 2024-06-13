97aa8c06e0d607c86b1724cdaeb4add2767835d1
diogomelo <demelodiogo@hotmail.com>
2024-06-13 11:17:37 +0100

    feito de novo o codigo no frontend

--- frontend/src/product/productLogic.js
+++ frontend/src/product/productLogic.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Product from "./product.js";
 import "./productLogic.css";
 import Config from "../config.js";
@@ -7,65 +7,32 @@ import Config from "../config.js";
 const ProductLogic = () => {
     const [loading, setLoading] = React.useState(true);
     const [products, setProducts] = React.useState([]);
-    const [error, setError] = React.useState(null);
 
-    React.useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const headers = {
-                    "Accept": "application/json",
-                    "x-access-token": Config.token
-                };
+    useEffect(() => {
+        fetch('/api/products', {
+            headers : {'Accept': 'application/json', 'x-access-token': Config.token}
+        })
+        .then ((response) => response.json())
+        .then ((response) => {
+            setLoading(false);
+            setProducts(response)
+        });
+        return () => setProducts([]);
+    }, [])
     
-                const response = await fetch("/api/product", {
-                    headers
-                });
-    
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-    
-                const data = await response.json();
-    
-                if (Array.isArray(data)) {
-                    setProducts(data);
-                } else {
-                    throw new Error('API response is not an array');
-                }
-            } catch (error) {
-                console.error('Fetch error:', error); // Log the error for debugging
-                setError(error.message);
-            } finally {
-                setLoading(false);
-            }
-        };
-    
-        fetchProducts();
-    
-        return () => setProducts([]); // Cleanup function
-    }, []);
-    
-    
-
-    
-
 
     if (loading) {
         return <h1>Loading...</h1>;
-    }
-
-    if (error) {
-        return <h1>Error: {error}</h1>;
     }
 
     return (
         <div className='products'>
             <label>Products: </label>
             <ul>
-                {products.map((product) => (
-                    <Product key={product._id} {...product} />
-                ))}
-            </ul>
+                {
+                    products.map((products) => <Product key={products._id} {...products} />)
+                }
+            </ul>   
         </div>
     );
 };
